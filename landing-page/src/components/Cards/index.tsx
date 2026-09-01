afc7712a40fc6ca1c4e265ef87364819badbb958
Luan481 <henrriqueluan973@gmail.com>
2026-08-31 21:31:17 -0300

    fix: background color

--- landing-page/src/components/Cards/index.tsx
+++ landing-page/src/components/Cards/index.tsx
@@ -4,13 +4,13 @@ interface CardProps {
     title: string;
     description: string;
     icon: React.ReactElement;
-    backround: string;
+    background: string;
 }
 
-export default function Cards({ title, description, icon, backround }: CardProps) {
+export default function Cards({ title, description, icon, background }: CardProps) {
     return (
         <div className="flex flex-col w-[350px] max-h-[230px] bg-white p-5 rounded-2xl border-1 border-[#ccc]">
-            <div className={`flex block ${backround} p-2 w-10 h-10 justify-center items-center rounded-2xl mt-3 mb-3`}>
+            <div className={`flex block ${background} p-2 w-10 h-10 justify-center items-center rounded-2xl mt-3 mb-3`}>
                 {icon}
             </div>
             
