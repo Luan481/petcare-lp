f9459b5e8fd2bfdccbadd05af5ba6f100323cffe
Luan481 <henrriqueluan973@gmail.com>
2026-09-15 20:54:22 -0300

    feat: busca por id no endpoint de animais

--- backend/src/routes/animais.routes.ts
+++ backend/src/routes/animais.routes.ts
@@ -1,10 +1,10 @@
 import { Router, Request, type Response } from "express";
 import { animaisService } from "../services/animais.service";
- 
+
 export const animaisRouter = Router()
 
 animaisRouter.get("/", async (_request: Request, response: Response) => {
-    try{
+    try {
         const res = await animaisService.getAll()
 
         response.json(res)
@@ -13,3 +13,25 @@ animaisRouter.get("/", async (_request: Request, response: Response) => {
     }
 
 })
+
+animaisRouter.get("/:id", async (request: Request<{ id: string }>, response: Response) => {
+    try {
+        const id = String(request.params.id);
+
+        if (!String(id)) {
+            return response.status(400).json({
+                message: "ID inválido",
+            });
+        }
+
+        const cliente = await animaisService.getById(id);
+
+        return response.json(cliente);
+    } catch (error) {
+        console.error(error);
+
+        return response.status(404).json({
+            message: "Cliente não encontrado",
+        });
+    }
+});
--- backend/src/services/animais.service.ts
+++ backend/src/services/animais.service.ts
@@ -5,9 +5,28 @@ class AnimaisService {
         try {
             const res = await pool.query("SELECT * FROM animais");
             console.log(res.rows)
-            return res.rows; 
+            return res.rows;
         } catch (error) {
             console.error("Erro ao buscar animais:", error);
+            throw new Error("Erro no banco de dados");
+        }
+    }
+    async getById(id: string) {
+        try {
+            const res = await pool.query(
+                "SELECT * FROM animais WHERE id = $1",
+                [id]
+            );
+
+            const animal = res.rows[0];
+
+            if (!animal) {
+                throw new Error("Animal não encontrado");
+            }
+
+            return animal;
+        } catch (error) {
+            console.error("Erro ao buscar animal por ID:", error);
             throw new Error("Erro no banco de dados");
         }
     }
